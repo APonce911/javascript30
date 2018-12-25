f584dcd5958db6bbf885ba342ab863a009d514d8
Airton Ponce <airtonponce@outlook.com>
2018-12-25 21:58:33 -0200

    add class on click

--- 01-javascript-drum-kit/drum.js
+++ 01-javascript-drum-kit/drum.js
@@ -1,21 +1,25 @@
-const key = document.querySelector(".key");
 
 function playAudio(event) {
   console.log(event.code);
-  let audio = document.querySelector(`audio[data-key=${event.code}]`);
+  const audio = document.querySelector(`audio[data-key=${event.code}]`);
   if (audio) {
     audio.currentTime = 0;
     audio.play();
   }
 }
 
-// const changekey = (event) => {
+function changeClass(event) {
+  const key = document.querySelector(`div[data-key=${event.code}]`);
   // insert class 'clicked' on key
+  key.classList.add('clicked');
   // timeout
+  // setTimeout(() => key.classList.remove('clicked') , 500);
   // remove class 'clicked'
-// };
+  // current.classList.remove('active');
+}
 
 document.addEventListener("keyup", playAudio);
+document.addEventListener("keyup", changeClass);
 
 // #TODO
 // 1- Change key style when clicked

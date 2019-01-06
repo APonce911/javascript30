967267be9c0b8ed76969674677d7e39bf89fd496
Airton Ponce <airtonponce@outlook.com>
2019-01-06 12:17:07 -0200

    set hourangle

--- 02-CSS-and-JS-clock/clock.js
+++ 02-CSS-and-JS-clock/clock.js
@@ -8,20 +8,23 @@ function setClock(){
   const day = new Date();
   const secondAngle = day.getSeconds()* 360/60 + 180;
   const minuteAngle = day.getMinutes()* 360/60 + 180;
+  const hourAngle = day.getHours()* 360/60 + 180;
+
 
   document.querySelector('.second').style.transform = `rotate(${secondAngle}deg)`;
   document.querySelector('.minute').style.transform = `rotate(${minuteAngle}deg)`;
-  return {secondAngle, minuteAngle}
+  document.querySelector('.hour').style.transform = `rotate(${hourAngle}deg)`;
+  return {day, secondAngle, minuteAngle, hourAngle}
 }
 // var minuteAngle = 0;
 // var hourAngle = 0;
 
 function updateClock(){
-  const day = new Date();
-
   const initialTime = setClock();
+  const day = initialTime.day;
   let secondAngle = initialTime.secondAngle
   let minuteAngle = initialTime.minuteAngle
+  // let hourAngle = initialTime.hourAngle
 
   console.log(initialTime)
   console.log(initialTime.secondAngle)

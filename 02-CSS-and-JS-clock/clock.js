8effd8253661c701c957cc1bd1b61b26f716043c
Airton Ponce <airtonponce@outlook.com>
2019-01-06 11:37:05 -0200

    plot pointers with style

--- 02-CSS-and-JS-clock/clock.js
+++ 02-CSS-and-JS-clock/clock.js
@@ -6,6 +6,9 @@
 // console.log(sec);
 
 var secondAngle = 0;
+// var minuteAngle = 0;
+// var hourAngle = 0;
+
 function updateClock(){
   var day = new Date();
   document.getElementById('time-number').innerText = day.getHours() + "-" + day.getMinutes() + "-" + day.getSeconds();
@@ -13,5 +16,7 @@ function updateClock(){
 
   setTimeout(updateClock, 1000);
   secondAngle += 360/60;
+  // minuteAngle += 360/(60*60);
+  // hourAngle += 360/(60*60*60);
 }
 updateClock();

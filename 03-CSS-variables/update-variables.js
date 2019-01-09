58a981f032415e207a09748ff1e05fef08d4d226
Airton Ponce <airtonponce@outlook.com>
2019-01-08 22:33:06 -0200

    define JS variables

---
+++ 03-CSS-variables/update-variables.js
@@ -0,0 +1,10 @@
+// document.querySelector('.second').style.transform = `rotate(${secondAngle}deg)`;
+let spacing = document.getElementById('spacing');
+let image = document.querySelector(`img`);
+
+// function updateSpacing() {
+//   image.style.marginTop = `${spacing.value}px`;
+//   image.style.marginLeft = `${spacing.value}px`;
+//   console.log(spacing.value);
+// }
+// spacing.addEventListener("change", updateSpacing);

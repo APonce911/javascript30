e03a1c411e516c38eb10364fd0c0a6ae9a7f5e15
Airton Ponce <airtonponce@outlook.com>
2019-01-08 22:33:29 -0200

    define callback function

--- 03-CSS-variables/update-variables.js
+++ 03-CSS-variables/update-variables.js
@@ -2,9 +2,9 @@
 let spacing = document.getElementById('spacing');
 let image = document.querySelector(`img`);
 
-// function updateSpacing() {
-//   image.style.marginTop = `${spacing.value}px`;
-//   image.style.marginLeft = `${spacing.value}px`;
-//   console.log(spacing.value);
-// }
+function updateSpacing() {
+  image.style.marginTop = `${spacing.value}px`;
+  image.style.marginLeft = `${spacing.value}px`;
+  console.log(spacing.value);
+}
 // spacing.addEventListener("change", updateSpacing);

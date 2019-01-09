223caf72fb9f97d18a653496fb0b2c70afbee2b7
Airton Ponce <airtonponce@outlook.com>
2019-01-08 22:40:33 -0200

    define blur JS variable

--- 03-CSS-variables/update-variables.js
+++ 03-CSS-variables/update-variables.js
@@ -1,11 +1,17 @@
 // document.querySelector('.second').style.transform = `rotate(${secondAngle}deg)`;
 let spacing = document.getElementById('spacing');
-let image = document.querySelector(`img`);
+let blur = document.getElementById('blur');
+const image = document.querySelector(`img`);
 
 function updateSpacing() {
   image.style.marginTop = `${spacing.value}px`;
   image.style.marginLeft = `${spacing.value}px`;
   console.log(spacing.value);
 }
-
+// function updateBlur() {
+  // image.style.filter = `blur(${blur.value}px)`;
+  // console.log(blur.value);
+// }
 spacing.addEventListener("change", updateSpacing);
+// blur.addEventListener("change", updateBlur);
+

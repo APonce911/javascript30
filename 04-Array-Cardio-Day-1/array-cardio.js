1871caa47b386230d3ad4cd28d8e339f051ce267
Airton Ponce <airtonponce@outlook.com>
2019-02-03 10:22:17 -0200

    log exercice objective

--- 04-Array-Cardio-Day-1/array-cardio.js
+++ 04-Array-Cardio-Day-1/array-cardio.js
@@ -22,16 +22,19 @@
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
-    console.log('hello')
-    // inventors.filter( x => console.log(x['year']))
+    console.log('Inventors born in the 1500\'s')
     const result = inventors.filter( x => x['year'] > 1500 && x['year'] < 1600 )
     console.log(result)
 
-
-
-
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
+
+
+
+
+
+
+
 
     // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
